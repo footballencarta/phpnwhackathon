cbf87c5d2547ed06f601bd7ec34b4009cb9aacc7
damon <damon@damonwilliams.co.uk>
2015-10-02 21:02:10 +0100

    Herp

--- html/dominos/app/Console/Importer.php
+++ html/dominos/app/Console/Importer.php
@@ -23,34 +23,19 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
+    protected function schedule(Schedule $schedule) {
         $schedule->call(function () {
-          $i = 126466800;
+          $i = 126468860;
 
           while ($i < PHP_INT_MAX) {
             $json = json_decode(file_get_contents('https://www.dominos.co.uk/Questionnaire/GetPizzaTrackerStatus?orderId=' . $i));
 
             print $i . PHP_EOL;
 
-            if($json->step1Css == 'is-step-in-progress') {
-              print '1 ' . $json->orderStage . PHP_EOL;
-            }
-
-            if($json->step2Css == 'is-step-in-progress') {
-              print '2 ' . $json->orderStage . PHP_EOL;
-            }
-
-            if($json->step3Css == 'is-step-in-progress') {
-              print '3 ' . $json->orderStage . PHP_EOL;
-            }
-
-            if($json->step4Css == 'is-step-in-progress') {
-              print '4 ' . $json->orderStage . PHP_EOL;
-            }
-
-            if($json->step5Css == 'is-step-in-progress') {
-              print '5 ' . $json->orderStage . PHP_EOL;
+            if($json->step1Css != 'is-step-in-progress') {
+              continue;
+              // Store as network
+              app('db')->insert('insert into pizza (order_id, current_order_status, created_at, updated_at) values (?, ?, NOW(), NOW())', [$i, 1]);
             }
 
             $i++;
